89999999ca9f9e96209177269564f8570097c39a
marshmelllo <robfarlow@gmail.com>
2016-08-19 15:28:06 -0400

    update

--- .idea/battleShip.py
+++ .idea/battleShip.py
@@ -3,29 +3,37 @@ from random import randint
 aiBoard = [["◻" for x in range(5)]for k in range(5)]
 userBoard = [["◻" for x in range(5)]for k in range(5)]
 userBoat = []
-aiboat = [randint(0, 5), randint(0, 5)]
+aiBoat = [randint(0, 5), randint(0, 5)]
+gameOver = false
+def aiGuess():
 
-def aiguess():
-    aiguess = [randint(0, 5),randint(0, 5)]
+    aiGuess = [randint(0, 5),randint(0, 5)]
     if userBoat == aiguess :
         print("You Lost")
+        gameOver = true
     else:
+        userBoard[aiGuess[0],aiGuess[1]] = "0"
         for i in range(5):
-            print(" ".join(aiboard[i]))
-
-
-
-def getUserPlacement(x,y):
-    userBoat = [x,y]
-    userBoard[x][y] = "X"
+            print(" ".join(userBoard[i]))
+    return
 
 def userGuess():
     for x in range(5):
-            print("  ".join(userBoard[x]))
+            print("  ".join(aiBoard[x]))
     print("guess a position")
     guess = input().split()
     guess = [int(p) for p in guess]
-    if guess == aiboat:
-        print("Game Over you win")
+    if guess == aiBoat:
+        print("You win")
+        gameOver = true
     else:
         userBoard[guess[0],guess[1]] = "0"
+    return
+
+print("Enter the location of your boat:")
+userBoat = input().split()
+userBoat = [int(k) for k in userBoat]
+userBoard[userBoat[0],userBoat[1]] = "B"
+while gameOver == false:
+    userGuess()
+    aiGuess()
