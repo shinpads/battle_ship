682e725bb84998fe88d15c16c05430e22dffe0ae
marshmelllo <robfarlow@gmail.com>
2016-08-19 14:57:54 -0400

    merge

--- .idea/battleShip.py
+++ .idea/battleShip.py
@@ -17,7 +17,21 @@ def aiguess():
         p
     return aiguess
 
+userBoard = [["◻" for x in range(5)]for k in range(5)]
+userBoat = []
+def getUserPlacement(x,y):
+    userBoat = [x,y]
+    userBoard[x][y] = "X"
 
+def userGuess():
+    for x in range(5):
+            print("  ".join(userBoard[x]))
+    print("guess a position")
+    guess = input().split()
+    guess = [int(p) for p in guess]
+    if guess == aiboat:
+        print("Game Over you win")
+    else:
+        userBoard[guess[0],guess[1]] = "0"
 
-
-
+userGuess()
