44119f606bcd58cd81669e92ee934d540b4616f6
mootgoat <david.41hz@protonmail.com>
2016-08-19 15:50:19 -0400

    fixes yo

--- .idea/battleShip.py
+++ .idea/battleShip.py
@@ -4,20 +4,22 @@ aiBoard = [["◻" for x in range(5)]for k in range(5)]
 userBoard = [["◻" for x in range(5)]for k in range(5)]
 userBoat = []
 aiBoat = [randint(0, 5), randint(0, 5)]
-gameOver = false
+print(aiBoat)
+gameOver = False
 def aiGuess():
 
-    aiGuess = [randint(0, 5),randint(0, 5)]
+    aiguess = [randint(0, 4),randint(0, 4)]
     if userBoat == aiguess :
         print("You Lost")
-        gameOver = true
+        gameOver = True
     else:
-        userBoard[aiGuess[0],aiGuess[1]] = "0"
+        userBoard[aiguess[0]][aiguess[1]] = "0"
         for i in range(5):
-            print(" ".join(userBoard[i]))
+            print("  ".join(userBoard[i]))
     return
 
 def userGuess():
+    print("----------------------------------------------------------------------------------------")
     for x in range(5):
             print("  ".join(aiBoard[x]))
     print("guess a position")
@@ -25,15 +27,19 @@ def userGuess():
     guess = [int(p) for p in guess]
     if guess == aiBoat:
         print("You win")
-        gameOver = true
+        gameOver = True
     else:
-        userBoard[guess[0],guess[1]] = "0"
+        aiBoard[guess[0]][guess[1]] = "0"
     return
 
 print("Enter the location of your boat:")
 userBoat = input().split()
 userBoat = [int(k) for k in userBoat]
-userBoard[userBoat[0],userBoat[1]] = "B"
-while gameOver == false:
+print(userBoat)
+userBoard[userBoat[0]][userBoat[1]]= "B"
+
+
+while gameOver == False:
     userGuess()
+
     aiGuess()
