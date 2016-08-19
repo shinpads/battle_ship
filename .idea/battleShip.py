e8d9a9b5350b0c63cc5de96c63ecfb7be009528c
marshmelllo <robfarlow@gmail.com>
2016-08-19 14:46:36 -0400

    UserGuess

--- .idea/battleShip.py
+++ .idea/battleShip.py
@@ -1,1 +1,44 @@
 """BattleShip game by Rob Farlow and David MacLean"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+userBoard = [["◻" for x in range(5)]for k in range(5)]
+userBoat = []
+def getUserPlacement(x,y):
+    userBoat = [x,y]
+    userBoard[x][y] = "X"
+
+def userGuess():
+    for x in range(5):
+            print("  ".join(userBoard[x]))
+    print("guess a position")
+    guess = input().split()
+    guess = [int(p) for p in guess]
+    if guess == aiboat:
+        print("Game Over you win")
+    else:
+        userBoard[guess[0],guess[1]] = "0"
+
+userGuess()
