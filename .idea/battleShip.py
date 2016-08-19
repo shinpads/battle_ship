ba8b5ed13f06d8801e81dfea7e62a86f6a3d50b8
marshmelllo <robfarlow@gmail.com>
2016-08-19 15:04:37 -0400

    organized

--- .idea/battleShip.py
+++ .idea/battleShip.py
@@ -1,24 +1,20 @@
 """BattleShip game by Rob Farlow and David MacLean"""
 from random import randint
+aiBoard = [["◻" for x in range(5)]for k in range(5)]
+userBoard = [["◻" for x in range(5)]for k in range(5)]
+userBoat = []
 aiboat = [randint(0, 5), randint(0, 5)]
-aiboard = []
-playerguess = [1,3]
-row = ["0","0","0","0","0"]
-def aiboard():
-    for i in range(5):
-        aiboard.append(row)
-        print(" ".join(aiboard[i]))
 
 def aiguess():
     aiguess = [randint(0, 5),randint(0, 5)]
     if userBoat == aiguess :
-        print("Fuck you! You Lost")
+        print("You Lost")
     else:
-        p
-    return aiguess
+        for i in range(5):
+            print(" ".join(aiboard[i]))
 
-userBoard = [["◻" for x in range(5)]for k in range(5)]
-userBoat = []
+
+
 def getUserPlacement(x,y):
     userBoat = [x,y]
     userBoard[x][y] = "X"
@@ -33,5 +29,3 @@ def userGuess():
         print("Game Over you win")
     else:
         userBoard[guess[0],guess[1]] = "0"
-
-userGuess()
